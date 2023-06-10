dd669b5ccc97dd7b9e8b57eb08b0ac7df5d5ca8c
Алексей <romalex.86@mail.ru>
2023-06-10 13:32:19 +0300

    initali

---
+++ 4.1.py
@@ -0,0 +1,19 @@
+'''def strcounter(s):  #N**2
+    for sym in s:
+        counter = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                counter += 1
+        print(sym, counter)
+
+strcounter('abckfaaaaS')'''
+
+def strcounter(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    for sym, count in syms_counter.items():
+        print(sym, count)
+
+strcounter('abckfaa')
